cba7d38b66589763685b3ae9b0a83fd0520c2cac
AthiraaAthi <athiraa2612@gmail.com>
2025-04-12 21:59:49 +0530

    Added initial implementation of ContactSelectionScreen

---
+++ lib/view/contact_selection_screen/contact_selection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ContactSelectionScreen extends StatelessWidget {
+  const ContactSelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
