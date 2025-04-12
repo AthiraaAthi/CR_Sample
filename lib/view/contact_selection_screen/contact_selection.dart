3207f519716b25fe75f87c70c86e08ac213ee04a
AthiraaAthi <athiraa2612@gmail.com>
2025-04-12 22:07:12 +0530

    Refactored ContactSelectionScreen to use StatefulWidget for future state management

--- lib/view/contact_selection_screen/contact_selection.dart
+++ lib/view/contact_selection_screen/contact_selection.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
 
-class ContactSelectionScreen extends StatelessWidget {
+class ContactSelectionScreen extends StatefulWidget {
   const ContactSelectionScreen({super.key});
 
+  @override
+  State<ContactSelectionScreen> createState() => _ContactSelectionScreenState();
+}
+
+class _ContactSelectionScreenState extends State<ContactSelectionScreen> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(child: Column());
