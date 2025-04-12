d2f62c4103fb8e42d49c8385e61c558da46a63a2
AthiraaAthi <athiraa2612@gmail.com>
2025-04-12 22:06:00 +0530

    Added route for RecordingListScreen in MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       home: HomeScreen(),
       routes: {
         '/contacts': (context) => ContactSelectionScreen(),
+        '/recordings': (context) => RecordingListScreen(),
       },
     );
   }
