a7ced279a924df804d21c1a17f817afe5e18e4d3
AthiraaAthi <athiraa2612@gmail.com>
2025-04-12 22:03:29 +0530

    Implemented RecordingListScreen with basic layout

--- lib/view/recording_list/recording_list.dart
+++ lib/view/recording_list/recording_list.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/cupertino.dart';
 
+class RecordingListScreen extends StatelessWidget {
+  const RecordingListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
