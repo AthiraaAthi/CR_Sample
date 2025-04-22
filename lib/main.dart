85c20e18655e382e0a2f7917a31723d8f75448c6
AthiraaAthi <athiraa2612@gmail.com>
2025-04-22 17:39:56 +0530

    Refactored stopAndSaveRecording function to ensure notification cancellation and logging after saving the recording

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,9 @@ Future<void> stopAndSaveRecording() async {
   final fileName = 'Recording_${DateTime.now().millisecondsSinceEpoch}.aac';
   final file = File('${folder.path}/$fileName');
   await file.writeAsBytes([]);
+  await flutterLocalNotificationsPlugin.cancel(0);
+  debugPrint('Recording saved to: ${file.path}');
+  await flutterLocalNotificationsPlugin.cancel(0);
 }
 
 void main() async {
