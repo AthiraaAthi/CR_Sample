2680bf2e585579ffa781413a4478b8ab1789dc25
AthiraaAthi <athiraa2612@gmail.com>
2025-04-23 17:46:10 +0530

    Added contacts permission check method in HomeScreen for future functionality

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,6 +12,10 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
+  }
+
+  Future<void> checkAndRequestContactsPermission() async {
+    PermissionStatus status = await Permission.contacts.status;
   }
 
   @override
